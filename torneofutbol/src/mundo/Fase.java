919fc5405375706e16b491b2a00fd1c3ad730dc4
Dario <hdcastanor@unal.edu.co>
2016-03-24 13:14:11 -0500

    Base de Datos lista! 83 Equipos y 1411 Jugadores disponibles

--- torneofutbol/src/mundo/Fase.java
+++ torneofutbol/src/mundo/Fase.java
@@ -1,5 +1,27 @@
 package mundo;
 
+import java.util.ArrayList;
+
 public class Fase {
+	public static final int PUNTOS_GANADOR=3;
+	public static final int PUNTOS_PERDEDOR=0;
+	public static final int PUNTOS_EMPATE=1;
+	private int codigoFase;
+	private int numeroGrupos;
+	private ArrayList<Grupo> listaEquipos;
+	private int tipoFase;
+	private String nombreFase;
+	private int codigoTorneo;
+	private boolean faseCompleta;
+	
+	/**
+	 * Este es el constructor de la clase Fase
+	 * @param listaEquipos
+	 * @param tipoFase
+	 */
+	public Fase(ArrayList<String> listaEquipos, int tipoFase) {
+		
+	}
+	
 
 }
--- torneofutbol/src/mundo/Grupo.java
+++ torneofutbol/src/mundo/Grupo.java
@@ -1,6 +1,48 @@
 package mundo;
 
+import java.util.ArrayList;
+
 public class Grupo {
 	
+	private String nombreGrupo;
+	private Equipo equipoA;
+	private Equipo equipoB;
+	private Equipo equipoC;
+	private Equipo equipoD;
+	private int codigoTorneo;
+	private int codigoGrupo;
+	private ArrayList<String> clasificadosTodosContraTodos;
+	
+	/**
+	 * Constructor de la clase Grupo
+	 * @param nombreGrupo
+	 * @param equipoA
+	 * @param equipoB
+	 * @param equipoC
+	 * @param equipoD
+	 * @param codigoTorneo
+	 */
+	public Grupo(String nombreGrupo, Equipo equipoA, Equipo equipoB, Equipo equipoC, Equipo equipoD, int codigoTorneo) {
+		super();
+		this.nombreGrupo = nombreGrupo;
+		this.equipoA = equipoA;
+		this.equipoB = equipoB;
+		this.equipoC = equipoC;
+		this.equipoD = equipoD;
+		this.codigoTorneo = codigoTorneo;
+	}
+	
+	public String getNombreGrupo() {
+		return "";
+	}
+	
+	/**
+	 * Este metodo debe jugar el todos contra todos y sacar dos clasificados
+	 * @return res
+	 */
+	public ArrayList<String> jugarTodosContraTodos() {
+		ArrayList res = new ArrayList<String>();
+		return res;
+	}
 
 }
